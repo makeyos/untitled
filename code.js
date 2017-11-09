5173ba81bd2f72e116b3623713c63fcfbc2201a3
makey <makeyos@gmail.com>
2017-11-09 23:41:29 +0000

    updated

--- code.js
+++ code.js
@@ -10,6 +10,9 @@ console.log("dzialam");
         existing_tabs = $('div.panel.panel-default').length;
         newEntry = $(currentEntry.clone()).appendTo(controlForm);
 
+        //all pannels addClass collapsed and show only new one
+        //change previous pannel to grean and change label
+
     newEntry.find('div#headingOne').attr('id','headingFour').find('a').prop('href','#collapseFour').text('Collasible #4').addClass('collapsed');
     newEntry.find('div#collapseOne').attr('id','collapseFour').removeClass('in')
 
